d65df7f47d642bcbe77d4b98e64d4cda2c27be53
OsmanovRuslan <ruslan.osmanov.2023@mail.ru>
2025-11-09 13:31:22 +0300

    Add merge-conflict

--- src/main/java/com/example/service/Main.java
+++ src/main/java/com/example/service/Main.java
@@ -3,5 +3,6 @@ package com.example.service;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Merge Conflict");
     }
 }
