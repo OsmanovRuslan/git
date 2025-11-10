e8f5c29ff2b4f7c64de811138025601159afcf5d
OsmanovRuslan <ruslan.osmanov.2023@mail.ru>
2025-11-10 14:26:14 +0300

    Update Main class

--- src/main/java/com/example/service/Main.java
+++ src/main/java/com/example/service/Main.java
@@ -4,5 +4,6 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Merge Conflict 2");
+        System.out.println("Rebase Example" );
     }
 }
