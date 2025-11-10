90b0fb84a2a658e481bdeb116f521f3709ac8bbb
OsmanovRuslan <ruslan.osmanov.2023@mail.ru>
2025-11-10 14:37:26 +0300

    Add RebaseI (updated message) (new update)
    Update RebaseI

--- src/main/java/com/example/service/Main.java
+++ src/main/java/com/example/service/Main.java
@@ -4,6 +4,6 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Merge Conflict 2");
-        System.out.println("Rebase Example" );
+        System.out.println("Update again");
     }
 }
---
+++ src/main/java/com/example/service/RebaseI.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+public class RebaseI {
+
+    static public void print() {
+        System.out.println("Rebase -I");
+    }
+
+}
